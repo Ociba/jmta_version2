ff9e88b0d32de476deb1a62fea75cfd5206c8ee1
microsoftjulius <julisema4@gmail.com>
2020-07-11 04:10:35 -0400

    chane

--- resources/views/Admin/my_revelations.blade.php
+++ resources/views/Admin/my_revelations.blade.php
@@ -15,7 +15,7 @@
             @include('layouts.sidebar')
             <div class="page-wrapper">
                 <div class="container-fluid">   <div class="alert alert-warning alert-dismissible fade show" role="alert">
-                    <strong>shalom, shalom, </strong> we are glad to you for the bible marathon
+                    <strong>shalom, shalom, </strong> we are glad to have you for the bible marathon
                     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
